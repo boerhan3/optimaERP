b33f3e4568833298aa1bd0e7af0a4893f2aadd29
Alim Burhanudin <boerhan3@yahoo.com>
2020-07-25 12:18:07 +0700

    Ubah Font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
         <!-- Theme style -->
         <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        {{-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
         <!-- Google Font: Source Sans Pro -->
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     </head>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,7 +16,7 @@
         <!-- Google Font: Source Sans Pro -->
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     </head>
-    <body class="hold-transition layout-top-nav">
+    <body class="hold-transition layout-top-nav" >
         <div class="wrapper">
             <!-- Navbar -->
             <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
@@ -24,7 +24,7 @@
                     <a href="http://optimac.co.id/" target="_blank" class="navbar-brand">
                         <img src="{{ asset('adminlte/dist/img/icon_logo_U3K_icon.ico') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                         style="opacity: .8">
-                        <span class="brand-text font-weight-light">OptimaERP</span>
+                        <span class="brand-text font-weight">OptimaERP</span>
                     </a>
 
                     <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -93,7 +93,7 @@
                     alt="optimaERP Logo"
                     class="brand-image img-circle elevation-3"
                     style="opacity: .8">
-                <span class="brand-text font-weight-light">OptimaERP</span>
+                <span class="brand-text font-weight">OptimaERP</span>
                 </a>
 
                 <!-- Sidebar -->
@@ -104,7 +104,7 @@
                         <img src={{ asset('adminlte/dist/img/user2-160x160.jpg') }} class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
                     </div>
                 </div>
                 <!-- Sidebar Menu -->
@@ -157,7 +157,7 @@
                 <!-- /.control-sidebar -->
 
                 <!-- Main Footer -->
-                <footer class="main-footer">
+                <footer class="main-footer text-xs">
                     <!-- To the right -->
                     <div class="float-right d-none d-sm-inline">
                         ERP Indonesia
